10e87d50a2162f6d861cdaa6881d39f0bdd441c4
Сергей Краснослободцев <72604889+SergeyKrasnoslobodtsev@users.noreply.github.com>
2025-05-31 10:25:05 +0300

    log

--- tests/test_fuzzy_search.py
+++ tests/test_fuzzy_search.py
@@ -1,28 +1,28 @@
 
-import unittest
-import common_test
+# import unittest
+# import common_test
 
-from src.PDFExtractor.scan_extractor import ScanExtractor
-from src.fuzzy_search import update_cell_text_in_document
-
-
-class Test_TestFuzzySearch(unittest.TestCase):
-
-    def setUp(self):
-        self.extractor_scan = ScanExtractor()
-
-    def test_update_text_to_cell(self):
-
-        pdf_bytes = common_test.get_pdf_scan()
-        doc = self.extractor_scan.extract(pdf_bytes)
-        new_doc = update_cell_text_in_document(doc, 0, 0, 8, 2, "10 000 000,00")
-        new_doc = update_cell_text_in_document(new_doc, 0, 0, 4, 1, "10 000 000,00")
-        new_doc = update_cell_text_in_document(new_doc, 0, 0, 4, 2, "10 000 000,00")
-        new_doc = update_cell_text_in_document(new_doc, 0, 0, 2, 4, "10 000 000,00")
-        new_doc = update_cell_text_in_document(new_doc, 0, 0, 5, 3, "10 000 000,00")
-        with open("updated_sample.pdf", "wb") as file:
-            file.write(new_doc.pdf_bytes)
+# from src.PDFExtractor.scan_extractor import ScanExtractor
+# from src.fuzzy_search import update_cell_text_in_document
 
 
-if __name__ == '__main__':
-    unittest.main()
+# class Test_TestFuzzySearch(unittest.TestCase):
+
+#     def setUp(self):
+#         self.extractor_scan = ScanExtractor()
+
+#     def test_update_text_to_cell(self):
+
+#         pdf_bytes = common_test.get_pdf_scan()
+#         doc = self.extractor_scan.extract(pdf_bytes)
+#         new_doc = update_cell_text_in_document(doc, 0, 0, 8, 2, "10 000 000,00")
+#         new_doc = update_cell_text_in_document(new_doc, 0, 0, 4, 1, "10 000 000,00")
+#         new_doc = update_cell_text_in_document(new_doc, 0, 0, 4, 2, "10 000 000,00")
+#         new_doc = update_cell_text_in_document(new_doc, 0, 0, 2, 4, "10 000 000,00")
+#         new_doc = update_cell_text_in_document(new_doc, 0, 0, 5, 3, "10 000 000,00")
+#         with open("updated_sample.pdf", "wb") as file:
+#             file.write(new_doc.pdf_bytes)
+
+
+# if __name__ == '__main__':
+#     unittest.main()
